1efb9961ddd967338a01646fe3316efb0f61bf25
Phaiax <phaiax@invisibletower.de>
2012-11-09 01:56:50 +0100

    Make some changes to make it work. Currently lacks on documentation, renaming the settings and testing the admin

--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -25,20 +25,20 @@ class CodeMirrorTextarea(forms.Textarea):
     CodeMirror:
         http://codemirror.net/
     """
+    def _media(self):
+        self.mode
 
-    class Media:
-        css = {}
-        js = (
-            r"%s/js/codemirror.js" % CODEMIRROR_PATH,
-        )
+        return forms.Media(css={'all': self.themepath},
+                           js = [r"%s/lib/codemirror.js" % CODEMIRROR_PATH, ] + self.jsmedia)
+    media = property(_media)
 
-    def __init__(self, attrs=None, path=None, parserfile=None, stylesheet=None, **kwargs):
+    def __init__(self, attrs=None, path=None, mode=None, theme=None, additional_configuration={}, ** kwargs):
         u"""Constructor of CodeMirrorTextarea
 
         Attribute:
             path          - CodeMirror directory URI (DEFAULT = settings.CODEMIRROR_PATH)
-            parserfile    - CodeMirror parserfile attribute (string or string array: DEFAULT = settings.CODEMIRROR_DEFAULT_PARSERFILE)
-            stylesheet    - CodeMirror stylesheet attribute (uri or uri array: DEFAULT = settings.CODEMIRROR_DEFAULT_STYLESHEET)
+            mode - CodeMirror parserfile attribute (string or string array: DEFAULT=settings.CODEMIRROR_DEFAULT_PARSERFILE)
+            theme - CodeMirror stylesheet attribute (uri or uri array: DEFAULT=settings.CODEMIRROR_DEFAULT_STYLESHEET)
 
         Example:
             *-------------------------------*
@@ -53,22 +53,51 @@ class CodeMirrorTextarea(forms.Textarea):
             CODEMIRROR_PATH = r"codemirror"
 
             codemirror = CodeMirrorTextarea(
-                # parserfile='parsexml.js',                                # Can be written as the left when only one file is needed.
-                parserfile=['parsexml.js'],
-                # stylesheet=r'xmlcolors.css'    # Can be written as the left when only one file is needed.
-                stylesheet=[r'css/xmlcolors.css'],
+                # mode='xml',                                # Can be written as the left when only one file is needed.
+                mode = {name: "javascript", json: true},
+                # theme=r'eclipse'    # Can be written as the left when only one file is needed.
+                theme = [r'eclipse'], # Old notation will work too
             )
             document = forms.TextField(widget=codemirror)
         """
         super(CodeMirrorTextarea, self).__init__(attrs=attrs, **kwargs)
-        self.path = path or settings.STATIC_URL + CODEMIRROR_PATH + '/js/'
-        self.parserfile = parserfile or CODEMIRROR_DEFAULT_PARSERFILE
-        self.stylesheet = stylesheet or CODEMIRROR_DEFAULT_STYLESHEET
-        self.stylesheet = [settings.STATIC_URL + CODEMIRROR_PATH + '/' + css for css in self.stylesheet]
-        if not hasattr(self.parserfile, '__iter__'):
-            self.parserfile = (self.parserfile,)
-        if not hasattr(self.stylesheet, '__iter__'):
-            self.stylesheet = (self.stylesheet,)
+        self.path = path or settings.STATIC_URL + CODEMIRROR_PATH + '/lib/'
+        self.mode = mode or CODEMIRROR_DEFAULT_PARSERFILE
+        self.theme = theme or CODEMIRROR_DEFAULT_STYLESHEET
+        self.additional_configuration = additional_configuration
+
+        if hasattr(self.mode, '__iter__'):
+            raise ValueError # Lists are no longer supported
+        if type(self.mode) is not dict:
+            self.mode = {'name': self.mode}
+
+        self.jsmedia = []
+        # Raise common error:
+        print self.mode
+        if self.mode['name'] == 'htmlmixed':
+            raise ValueError('See codemirror Dokumentation. You have to use "text/html" instead.')
+        # Expand shortterms
+        if self.mode['name'] == 'text/html':
+            self.jsmedia = self.jsmedia + ['xml', 'javascript', 'css', 'htmlmixed']
+        else:
+            self.jsmedia = self.jsmedia + [self.mode['name']]
+
+        self.jsmedia = [settings.STATIC_URL + CODEMIRROR_PATH + '/mode/' + n + "/" + n + ".js" for n in self.jsmedia]
+
+        ## Convert theme to list and back to string
+        if not hasattr(self.theme, '__iter__'):
+            if ' ' in self.theme:
+                self.theme = self.theme.split(' ')
+            else:
+                self.theme = [self.theme, ]
+        self.themepath = list(self.theme)
+        ## Default theme has got an other path base
+        if 'default' in self.theme:
+            self.themepath.remove('default')
+        self.themepath = [settings.STATIC_URL + CODEMIRROR_PATH + '/theme/' + css + ".css" for css in self.themepath]
+        self.themepath = self.themepath + [settings.STATIC_URL + CODEMIRROR_PATH + '/lib/codemirror.css']
+        if hasattr(self.theme, '__iter__'):
+            self.theme = ' '.join(self.theme)
 
     def render(self, name, value, attrs=None):
         u"""Render CodeMirrorTextarea"""
@@ -76,8 +105,9 @@ class CodeMirrorTextarea(forms.Textarea):
         kwargs = {
             'id': '"id_%s"' % name,
             'path': json.dumps(self.path),
-            'parserfile': json.dumps(self.parserfile),
-            'stylesheet': json.dumps(self.stylesheet),
+            'mode': json.dumps(self.mode),
+            'theme': json.dumps(self.theme),
+            'additional_configuration': ",\n\r     ".join(['%s :%s' % i for i in self.additional_configuration.items()]),
         }
         for key in kwargs.keys():
             kwargs[key] = mark_safe(kwargs[key])
@@ -93,6 +123,6 @@ class AdminCodeMirrorTextareaWidget(CodeMirrorTextarea, AdminTextareaWidget):
 
 class AdminHTMLEditor(AdminCodeMirrorTextareaWidget):
     def __init__(self, *args, **kwargs):
-        kwargs['parserfile'] = ["parsexml.js", "parsecss.js", "tokenizejavascript.js", "parsejavascript.js", "parsehtmlmixed.js"]
-        kwargs['stylesheet'] = ["css/xmlcolors.css", "css/jscolors.css", "css/csscolors.css"]
+        kwargs['mode'] = ["xml", "css", "javascript", "htmlmixed"]
+        kwargs['theme'] = ["xml", "js", "css"]
         super(AdminHTMLEditor, self).__init__(*args, **kwargs)
