1ed13b48c29e87dbbcae50720087b62900a28e79
Phaiax <phaiax@invisibletower.de>
2012-11-09 20:35:03 +0100

    Make the widget more flexible Update inline documentation

--- codemirror/widgets.py
+++ codemirror/widgets.py
@@ -15,8 +15,8 @@ from django.utils.safestring import mark_safe
 CODEMIRROR_PATH = getattr(settings, 'CODEMIRROR_PATH', 'codemirror')
 if CODEMIRROR_PATH.endswith('/'):
     CODEMIRROR_PATH = CODEMIRROR_PATH[:-1]
-CODEMIRROR_DEFAULT_PARSERFILE = getattr(settings, 'CODEMIRROR_DEFAULT_PARSERFILE', 'parsedummy.js')
-CODEMIRROR_DEFAULT_STYLESHEET = getattr(settings, 'CODEMIRROR_DEFAULT_STYLESHEET', '')
+CODEMIRROR_DEFAULT_MODE = getattr(settings, 'CODEMIRROR_DEFAULT_MODE', '')
+CODEMIRROR_DEFAULT_THEME = getattr(settings, 'CODEMIRROR_DEFAULT_THEME', 'default')
 
 
 class CodeMirrorTextarea(forms.Textarea):
@@ -26,88 +26,130 @@ class CodeMirrorTextarea(forms.Textarea):
         http://codemirror.net/
     """
     def _media(self):
-        self.mode
-
-        return forms.Media(css={'all': self.themepath},
-                           js = [r"%s/lib/codemirror.js" % CODEMIRROR_PATH, ] + self.jsmedia)
+        return forms.Media(css={'all': self.cssfiles},
+                           js=[r"%s/lib/codemirror.js" % CODEMIRROR_PATH, ] + self.jsmedia)
     media = property(_media)
 
-    def __init__(self, attrs=None, path=None, mode=None, theme=None, additional_configuration={}, ** kwargs):
+    # What media do we need to get a certain mode?
+    mode_mimetype_media_association = {'text/html': ['xml', 'javascript', 'css', 'htmlmixed'],
+                                       'htmlmixed': ['xml', 'javascript', 'css', 'htmlmixed']}
+
+    # Some js files have additional stylesheets
+    js_files_with_stylesheets = ['dialog', 'simple-hint']
+
+    def __init__(self, attrs=None, mode=None, theme=None, utils=[], additional_configuration={}, additional_css="", additional_js="", ** kwargs):
         u"""Constructor of CodeMirrorTextarea
-
-        Attribute:
-            path          - CodeMirror directory URI (DEFAULT = settings.CODEMIRROR_PATH)
-            mode - CodeMirror parserfile attribute (string or string array: DEFAULT=settings.CODEMIRROR_DEFAULT_PARSERFILE)
-            theme - CodeMirror stylesheet attribute (uri or uri array: DEFAULT=settings.CODEMIRROR_DEFAULT_STYLESHEET)
-
+        @keyword mode: CodeMirror mode attribute (string (for MIME and Mode) or dict (for Mode with options): DEFAULT=settings.CODEMIRROR_DEFAULT_MODE)
+        @keyword theme: CodeMirror theme attribute (string or space separated string: DEFAULT=settings.CODEMIRROR_DEFAULT_THEME)
+        @keyword utils: List of utils to load from the lib / utils directory. Css Files will be included
+        @keyword additional_configuration: Additional Parameters for the JS - Codemirror Constructor. Pay attention to proper escaping
+        @keyword additional_css: Css to style this specific editarea. Use $$name$$ as alias for the textareas name
+        @keyword additional_js: Js to be executed after CodeMirror.fromTextArea(...). Usefull for defining new modes.
+                Use $$id$$ as alias for the textareas id, for example: document.getElementById("$$id$$")
+                The Editor object is available as window.$$id$$ in javascript
         Example:
-            *-------------------------------*
-            + static
-              + codemirror
-                + css
-                  - xmlcolors.css
-                + js
-                  - codemirror.js
-                  - parsexml.js
-            *-------------------------------*
+            * - - - - - - - - - - - - - - - - - - - - - - - - - -SETTINGS *
             CODEMIRROR_PATH = r"codemirror"
+            # Add codemirror to INSTALLED_APPS for automatic import of static files (collectstatic)
+            * - - - - - - - - - - - - - - - - - - - - - - - - - - - -FORM *
+            from django import forms
+            from codemirror.widgets import CodeMirrorTextarea
+            class FileEditForm(forms.Form):
+                ## Pay attention:
+                ## additional_configuration and mode make different escaping approaches (otherwise js-functions would be
+                ## forwarded as strings)
+                codemirror = CodeMirrorTextarea(
+                        mode='text/html',
+                        theme = 'ambiance',
+                        utils = ['search', 'searchcursor', 'dialog'],
+                        additional_configuration={'lineNumbers': 'true', 'lineWrapping': 'true', 'autofocus': 'true',
+                                                    'example_string_property': '"value"',
+                                                    'indentUnit': '4', 'onKeyEvent': '''function(editor, event){
+                                                        event = $.event.fix(event); // Jquery available
+                                                        if (event.type == "keypress" && event.altKey && event.which == 114)
+                                                        {
+                                                             editor.save();
+                                                             $(editor.getTextArea()).parents('form').submit();
+                                                             return true;
+                                                        }
+                                                    }'''}
+                        )
+                file_content = forms.CharField(widget=codemirror)
+                def __init__(self, mode='text/html', *args, **kwargs):
+                    super(FileEditForm, self).__init__(*args, **kwargs)
+                    ## Update Widget preferences by constructor variables
+                    self.fields['file_content'].widget.mode = mode
+                    # or for example
+                    self.fields['file_content'].widget.mode = {"name": "javascript", "json": True}
+                    # you can apply styles to this instance only 
+                    self.fields['file_content'].widget.additional_css = '''
+                        textarea#$$id$$ ~ .Codemirror .CodeMirror-selected {
+                            background-color: blue;
+                        }'''
+                    # After Changing mode or utils or theme you should call
+                    self.fields['file_content'].widget.refresh_media()
+            * - - - - - - - - - - - - - - - - - - - - - - - - - - - -TEMPATE *
+            Use this in your < head > tags to include the needed media. Use { % block % }.
+            {{ form.media.js }}
+            {{ form.media.css }}
+            * - - - - - - - - - - - - - - - - - - - - - - - - - - - -END *
 
-            codemirror = CodeMirrorTextarea(
-                # mode='xml',                                # Can be written as the left when only one file is needed.
-                mode = {name: "javascript", json: true},
-                # theme=r'eclipse'    # Can be written as the left when only one file is needed.
-                theme = [r'eclipse'], # Old notation will work too
-            )
-            document = forms.TextField(widget=codemirror)
         """
         super(CodeMirrorTextarea, self).__init__(attrs=attrs, **kwargs)
-        self.path = path or settings.STATIC_URL + CODEMIRROR_PATH + '/lib/'
-        self.mode = mode or CODEMIRROR_DEFAULT_PARSERFILE
-        self.theme = theme or CODEMIRROR_DEFAULT_STYLESHEET
+        self.mode = mode or CODEMIRROR_DEFAULT_MODE
+        self.theme = theme or CODEMIRROR_DEFAULT_THEME
         self.additional_configuration = additional_configuration
+        self.utils = utils
+        self.additional_css = additional_css
+        self.additional_js = additional_js
+        self.refresh_media()
 
-        if hasattr(self.mode, '__iter__'):
+    def refresh_media(self):
+        # Mode can be string or dict. For Mimetypes (eg text/html), codemirror can't handle dicts
+        if type(self.mode) is list or type(self.mode) is tuple:
             raise ValueError # Lists are no longer supported
-        if type(self.mode) is not dict:
-            self.mode = {'name': self.mode}
-
-        self.jsmedia = []
-        # Raise common error:
-        print self.mode
-        if self.mode['name'] == 'htmlmixed':
-            raise ValueError('See codemirror Dokumentation. You have to use "text/html" instead.')
-        # Expand shortterms
-        if self.mode['name'] == 'text/html':
-            self.jsmedia = self.jsmedia + ['xml', 'javascript', 'css', 'htmlmixed']
+        if type(self.mode) is dict:
+            self.modename = self.mode['name']
         else:
-            self.jsmedia = self.jsmedia + [self.mode['name']]
+            self.modename = self.mode
 
+        if self.modename in self.mode_mimetype_media_association.keys():
+            self.jsmedia = self.mode_mimetype_media_association[self.modename]
+        elif '/' in self.modename:
+            raise NotImplementedError("Currently, not all MIME-Types are supported in django-codmirror-widget. Please update" +
+            "CodeMirrorTextarea.mode_mimetype_media_association to get your MIME working. Please forward changes upstream.")
+        else:
+            self.jsmedia = [self.modename]
         self.jsmedia = [settings.STATIC_URL + CODEMIRROR_PATH + '/mode/' + n + "/" + n + ".js" for n in self.jsmedia]
 
-        ## Convert theme to list and back to string
+        self.jsmedia = self.jsmedia + [settings.STATIC_URL + CODEMIRROR_PATH + '/lib/util/' + n + ".js" for n in self.utils]
+
+        ## Convert theme to list ...*
         if not hasattr(self.theme, '__iter__'):
             if ' ' in self.theme:
                 self.theme = self.theme.split(' ')
             else:
                 self.theme = [self.theme, ]
-        self.themepath = list(self.theme)
-        ## Default theme has got an other path base
-        if 'default' in self.theme:
-            self.themepath.remove('default')
-        self.themepath = [settings.STATIC_URL + CODEMIRROR_PATH + '/theme/' + css + ".css" for css in self.themepath]
-        self.themepath = self.themepath + [settings.STATIC_URL + CODEMIRROR_PATH + '/lib/codemirror.css']
+        self.cssfiles = list(self.theme)
+        self.cssfiles = [settings.STATIC_URL + CODEMIRROR_PATH + '/theme/' + css + ".css" for css in self.cssfiles if css != 'default']
+        self.cssfiles = self.cssfiles + [settings.STATIC_URL + CODEMIRROR_PATH + '/lib/codemirror.css']
+        self.cssfiles = self.cssfiles + [settings.STATIC_URL + CODEMIRROR_PATH + '/lib/util/' + n + ".css"
+                                         for n in self.utils if n in self.js_files_with_stylesheets]
+        ## *... and back to string
         if hasattr(self.theme, '__iter__'):
             self.theme = ' '.join(self.theme)
 
     def render(self, name, value, attrs=None):
         u"""Render CodeMirrorTextarea"""
         html = super(CodeMirrorTextarea, self).render(name, value, attrs)
+        id = 'id_%s' % name
         kwargs = {
-            'id': '"id_%s"' % name,
-            'path': json.dumps(self.path),
+            'id': id,
             'mode': json.dumps(self.mode),
-            'theme': json.dumps(self.theme),
+            'theme': '"%s"' % (self.theme,),
             'additional_configuration': ",\n\r     ".join(['%s :%s' % i for i in self.additional_configuration.items()]),
+            'additional_js': self.additional_js.replace('$$id$$', id),
+            'additional_css': self.additional_css.replace('$$id$$', id),
         }
         for key in kwargs.keys():
             kwargs[key] = mark_safe(kwargs[key])
